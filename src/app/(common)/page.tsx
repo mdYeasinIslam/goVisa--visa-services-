231f9fc0fcd34e0eec624d3ed416eb738efaeba4
mdYeasinIslam <hyeasinislam@gamil.com>
2025-08-16 23:55:59 +0600

    auth pages added and visa service page design with fake data

--- src/app/(common)/page.tsx
+++ src/app/(common)/page.tsx
@@ -1,6 +1,8 @@
 import Banner from "@/components/home/Banner";
 import HeroSection from "@/components/home/HeroSection";
 import HowItWorks from "@/components/home/how-it-works";
+import OurServices from "@/components/home/OurServices";
+import VisaServicesListing from "@/components/visa-serivices/VisaServices";
 import Image from "next/image";
 
 export default function Home() {
@@ -8,7 +10,8 @@ export default function Home() {
     <main>
       {/* <Banner /> */}
       <HeroSection />
-      <HowItWorks/>
+      <OurServices/>
+      <HowItWorks />
    </main>
   );
 }
